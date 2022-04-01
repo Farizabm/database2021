01f9d745b1ce7ae8572f1c550c49fce8de037d81
Farizabm <78656147+Farizabm@users.noreply.github.com>
2022-04-01 21:55:14 +0600

    Add files via upload

---
+++ Task1/1/цикл for/j.py
@@ -0,0 +1,7 @@
+n= int(3)
+a = list(map(int, input().split()))
+cnt=int(0)
+for i in range(1,n+1):
+    cnt +=i
+print(cnt)
+    
---
+++ Task1/1/цикл for/k.py
@@ -0,0 +1,6 @@
+n = int(input())
+s = 0
+for i in range(n):
+    temp = int(input())
+    s += temp
+print(s)
