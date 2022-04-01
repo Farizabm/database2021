fb4fe49d21e9f9bdbe01ca271ae9f363074ff82b
Farizabm <78656147+Farizabm@users.noreply.github.com>
2022-04-01 21:54:06 +0600

    Add files via upload

---
+++ Task1/1/массивы/e.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list(map(int, input().split()))
+for i in range(1,len(arr)-1):
+    if arr[i]>=0 and arr[i-1>=0 and arr[i+1]>=0:
+        print("YES")
+    else if arr[i]<0 and arr[i-1<0 and arr[i+1]<0:
+        print("YES")
+    else:
+        print("NO")
+    
+                          
